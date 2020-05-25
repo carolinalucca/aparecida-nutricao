918d727f25a24b3c60a88f8f181f178d619116e5
Carolina Lucca <carolinaplucca@gmail.com>
2020-05-25 14:54:48 -0300

    [@carolinalucca] calculate imc

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,3 +1,23 @@
 var titulo = document.querySelector(".titulo"); 	
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+pacientes.forEach (paciente => {
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    if (peso <= 0 || peso >= 1000) {
+        tdImc.textContent = "Peso inválido!";
+    } else if (altura <= 0 || altura >= 3.00) {
+        tdImc.textContent = "Altura inválida!";
+    } else {
+        var imc = peso/(altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+});
